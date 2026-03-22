3e316b5d05efa71e89e319546fd6eb702e65ba85
infantakash210705 <infantakash260@gmail.com>
2026-03-22 15:14:42 -0400

    fix merge conflict in app.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const express = require("express")
-
-const app = express()
+const express = require("express");
+const app = express();
 
 app.get("/", (req,res)=>{
-<<<<<<< HEAD
-    res.send("Jenkins Pipeline Project is Running Successfully")
-=======
-    res.send("Jenkins Pipeline Project is Good Way Running")
->>>>>>> 746023e506937aa2a498675b5bfd8e673bcda0e0
-})
+    res.send("Pipeline App Running 🚀");
+});
 
 app.listen(3000, ()=>{
-    console.log("Server running on port 3000")
-})
+    console.log("Server running on port 3000");
+});
