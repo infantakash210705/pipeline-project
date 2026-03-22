746023e506937aa2a498675b5bfd8e673bcda0e0
INFANT AKASH W <infantakash260@gmail.com>
2026-03-22 14:17:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req,res)=>{
-    res.send("Jenkins Pipeline Project Running")
+    res.send("Jenkins Pipeline Project is Good Way Running")
 })
 
 app.listen(3000, ()=>{
