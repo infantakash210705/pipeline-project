9aff19f94b86e26cace13766842c1c32e3c9dbdd
INFANT AKASH W <infantakash260@gmail.com>
2026-03-22 13:57:40 -0400

    Update response message in root route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req,res)=>{
-    res.send("Jenkins Pipeline Project is Running Successfully")
+    res.send("Jenkins Pipeline Project Running")
 })
 
 app.listen(3000, ()=>{
